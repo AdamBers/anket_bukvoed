20c130b0fb248b5892e7d3e83da96425a8652938
adam <adambers2020@gmail.com>
2022-11-29 09:32:02 +0300

    added styles

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function checkRange(event) {
+    var fourthQuestion = document.getElementById("fourth-question")
+    if (event.target.value >= 6) {
+        fourthQuestion.style.display = "none";
+    }
+    if (event.target.value <= 5) {
+        fourthQuestion.style.display = "block";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
